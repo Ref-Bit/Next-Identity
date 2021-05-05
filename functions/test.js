2bb1346b22676eabe4f32f7d56f3e65a7f942e47
Ref-Bit <rsv.gho@gmail.com>
2021-05-05 16:31:11 +0300

    Added test cloud function

---
+++ functions/test.js
@@ -0,0 +1,14 @@
+exports.handler = async () => {
+  console.log('Handler Ran...');
+
+  const data = {
+    name: 'mario',
+    age: 35,
+    job: 'plumber',
+  };
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(data),
+  };
+};
